6f647e257c612c5f31a5bf5bea80b64a97c73731
me3za <medkrix1@gmail.com>
2024-03-29 13:29:31 +0000

    Landing page styling updates

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom';
 
 function LandingPage() {
 	return (
-		<div className="flex flex-col justify-between landing-page base-page px-10 md:px-[10%]">
+		<div className="flex flex-col justify-between landing-page h-screen w-screen px-10 md:px-[10%]">
 			<header className="pt-10 flex justify-between">
 				<details className=" dropdown md:hidden">
-					<summary className="rounded-md btn focus:outline focus:outline-2 focus:outline-accent bg-transparent border-2 border-accent text-primary-content hover:bg-accent hover:text-primary-content">
+					<summary className="rounded-md btn bg-transparent border-2 border-accent text-primary-content hover:border-accent hover:bg-accent hover:text-primary-content">
 						More Links
 					</summary>
 					<ul className="p-2 shadow menu dropdown-content z-[1] rounded-md bg-primary border-2 border-accent w-52">
@@ -31,14 +31,14 @@ function LandingPage() {
 				</details>
 				<ul className="hidden items-end text-2xl gap-4 font-semibold md:flex">
 					<li>
-						<a className="flex gap-2 text-primary-content" href="/">
+						<a className="flex gap-2 text-primary-content hover:brightness-110 transition-all" href="/">
 							<img src="/dtb.svg" alt="logo" className="h-8" />
 							Dtboard
 						</a>
 					</li>
 					<li>
 						<a
-							className="text-stone-500"
+							className="text-stone-500 hover:text-primary-content transition-all"
 							href="https://www.github.com/me3zaAKAgoat"
 							target="_blank"
 						>
@@ -47,7 +47,7 @@ function LandingPage() {
 					</li>
 					<li>
 						<a
-							className="text-stone-500"
+							className="text-stone-500 hover:text-primary-content transition-all"
 							href="https://www.me3za.tech/posts/dtb/"
 							target="_blank"
 						>
@@ -57,7 +57,7 @@ function LandingPage() {
 				</ul>
 				<Link
 					to="/login"
-					className="flex items-center justify-center px-4 text-lg font-bold rounded-md w-24 h-10 focus:outline focus:outline-2 focus:outline-accent bg-transparent border-2 border-accent text-primary-content hover:bg-accent hover:text-primary-content"
+					className="transition-all flex items-center justify-center px-4 text-lg font-bold rounded-md w-24 h-10 focus:outline focus:outline-2 focus:outline-accent bg-transparent border-2 border-accent text-primary-content hover:bg-accent hover:text-primary-content"
 				>
 					Log In
 				</Link>
@@ -79,19 +79,19 @@ function LandingPage() {
 						find peace of mind, whether at work or school.
 					</p>
 					<Link
-						className="flex items-center justify-center text-xl h-12 w-28 font-bold rounded-md border-accent focus:outline focus:outline-2 focus:outline-primary-content bg-accent text-primary-content hover:bg-primary-content hover:text-accent"
+						className="transition-all flex items-center justify-center text-xl h-12 w-28 font-bold rounded-md border-accent focus:outline focus:outline-2 focus:outline-primary-content bg-accent text-primary-content hover:bg-primary-content hover:text-accent"
 						to="/register"
 					>
 						Sign Up
 					</Link>
 				</div>
 				<img
-					className="md:w-1/2 object-contain"
+					className="md:w-1/2 object-contain mb-14"
 					src="/hud.png"
 					alt="heads up display showcase image"
 				/>
 			</main>
-			<footer className="pb-10 flex justify-center text-stone-400 text-sm font-semibold">
+			<footer className="pb-6 flex justify-center text-stone-400 text-sm font-normal">
 				© 2024 Me3za. All rights reserved
 			</footer>
 		</div>
