936fea211bf84bfbaccdf7231d51162bf007d9c6
me3za <medkrix1@gmail.com>
2024-03-31 13:54:20 +0000

    Fixed colors issues originating from tailwind config file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,24 +1,28 @@
+const colors = {
+	primary: '#0f0f0f',
+	'primary-content': '#c9c9c9',
+	secondary: '#181818',
+	'secondary-content': '#c9c9c9',
+	tertiary: '#242424',
+	'tertiary-content': '#c9c9c9',
+	quaternary: '#4a4a4a',
+	'quaternary-content': '#c9c9c9',
+	accent: '#8957e5',
+	'accent-content': '#0f0f0f',
+	neutral: '#3b3b3b',
+	info: '#67e8f9',
+	success: '#23a55a',
+	warning: '#fde047',
+	error: '#F23F43',
+};
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {
 			colors: {
-				primary: '#0f0f0f',
-				'primary-content': '#c9c9c9',
-				secondary: '#181818',
-				'secondary-content': '#c9c9c9',
-				tertiary: '#242424',
-				'tertiary-content': '#c9c9c9',
-				quaternary: '#4a4a4a',
-				'quaternary-content': '#c9c9c9',
-				accent: '#8957e5',
-				'accent-content': '#0f0f0f',
-				neutral: '#3b3b3b',
-				info: '#67e8f9',
-				success: '#307530',
-				warning: '#fde047',
-				error: '#F23F43',
+				...colors,
 			},
 		},
 	},
@@ -26,17 +30,7 @@ export default {
 		themes: [
 			{
 				mytheme: {
-					primary: 'colors.primary',
-					'primary-content': 'colors.primary-content',
-					secondary: 'colors.secondary',
-					'secondary-content': 'colors.secondary-content',
-					accent: 'colors.accent',
-					'accent-content': 'colors.accent-content',
-					neutral: 'colors.neutral',
-					info: 'colors.info',
-					success: 'colors.success',
-					warning: 'colors.warning',
-					error: 'colors.error',
+					...colors,
 				},
 			},
 		],
